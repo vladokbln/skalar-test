1bf6d1c67ffd3dfc97a2ada6a2784ca52132ae58
vladokbln <vladokbln@gmail.com>
2020-01-31 16:21:51 +0200

    продолжаем верстать header

--- gulp/tasks/sprite-png.js
+++ gulp/tasks/sprite-png.js
@@ -5,7 +5,7 @@ module.exports = function (){
 		var spriteData = $.gulp.src('./src/static/img/icon/*.png')
 		.pipe(spritesmith({
 			imgName: 'sprite.png',
-			cssName: 'sprite.less',
+			cssName: 'sprite.scss',
 			imgPath: '../img/sprite/sprite.png'
 		}));
 		return spriteData.pipe($.gulp.dest('./src/static/img/sprite'));
