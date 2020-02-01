dbfb45051fb054ef573485756429d1fbc4d8d4d3
vladokbln <vladokbln@gmail.com>
2020-02-01 19:06:50 +0200

    верстка main breadcrumbs

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,4 +1,11 @@
 // поддежрка подключения svg во всех браузерах
 $(document).ready(function () {
 	svg4everybody();
+	var sandwichClick = function() {
+		$(document).on('click', '.catalog-nav__header', function(){
+			var sandwich = $(this).find('.sandwich');
+			sandwich.toggleClass('is-active')
+		})
+	}
+	sandwichClick();
 });
