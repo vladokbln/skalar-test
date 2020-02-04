9acb4136040df877b9b98ad7e10e06a6536f1460
vladokbln <vladokbln@gmail.com>
2020-02-04 13:02:12 +0200

    Верстка category

--- gulp/tasks/script.js
+++ gulp/tasks/script.js
@@ -1,39 +1,51 @@
-const uglify = require('gulp-uglify'),
-    	concat = require('gulp-concat');
+const uglify = require("gulp-uglify"),
+  concat = require("gulp-concat");
 
-module.exports = function (){
-	$.gulp.task('libs-js:dev', function () {
-		return $.gulp.src([
-			'node_modules/jquery/dist/jquery.min.js',
-			'node_modules/svg4everybody/dist/svg4everybody.min.js'
-		]) //через запятую перечисление библиотек
-			.pipe(concat('libs.js'))
-			.pipe($.gulp.dest('build/js/'))
-			.pipe($.webserver.reload({
-				stream: true
-			})); 
-	});
-	$.gulp.task('libs-js:build', function () {
-		return $.gulp.src([
-			'node_modules/jquery/dist/jquery.js',
-			'node_modules/svg4everybody/dist/svg4everybody.min.js'
-		]) //через запятую перечисление библиотек
-			.pipe(concat('libs.js'))
-			.pipe(uglify())
-			.pipe($.gulp.dest('build/js/'))
-	});
-	$.gulp.task('scripts:dev', function () {
-		return $.gulp.src(['src/static/js/*.js','!src/static/js/libs.js'])
-			.pipe(concat('main.js'))
-			.pipe($.gulp.dest('build/js/'))
-			.pipe($.webserver.reload({
-				stream: true
-			}));
-	});
-	$.gulp.task('scripts:build', function () {
-		return $.gulp.src(['src/static/js/*.js','!src/static/js/libs.js'])
-			.pipe(concat('main.js'))
-			.pipe(uglify())
-			.pipe($.gulp.dest('build/js/'))
-	});
-}
+module.exports = function() {
+  $.gulp.task("libs-js:dev", function() {
+    return $.gulp
+      .src([
+        "node_modules/jquery/dist/jquery.min.js",
+        "node_modules/svg4everybody/dist/svg4everybody.min.js",
+        "node_modules/nouislider/distribute/nouislider.js",
+        "node_modules/wnumb/wNumb.js"
+      ]) //через запятую перечисление библиотек
+      .pipe(concat("libs.js"))
+      .pipe($.gulp.dest("build/js/"))
+      .pipe(
+        $.webserver.reload({
+          stream: true
+        })
+      );
+  });
+  $.gulp.task("libs-js:build", function() {
+    return $.gulp
+      .src([
+        "node_modules/jquery/dist/jquery.js",
+        "node_modules/svg4everybody/dist/svg4everybody.min.js",
+        "node_modules/nouislider/distribute/nouislider.js",
+        "node_modules/wnumb/wNumb.js"
+      ]) //через запятую перечисление библиотек
+      .pipe(concat("libs.js"))
+      .pipe(uglify())
+      .pipe($.gulp.dest("build/js/"));
+  });
+  $.gulp.task("scripts:dev", function() {
+    return $.gulp
+      .src(["src/static/js/*.js", "!src/static/js/libs.js"])
+      .pipe(concat("main.js"))
+      .pipe($.gulp.dest("build/js/"))
+      .pipe(
+        $.webserver.reload({
+          stream: true
+        })
+      );
+  });
+  $.gulp.task("scripts:build", function() {
+    return $.gulp
+      .src(["src/static/js/*.js", "!src/static/js/libs.js"])
+      .pipe(concat("main.js"))
+      .pipe(uglify())
+      .pipe($.gulp.dest("build/js/"));
+  });
+};
--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,11 +1,62 @@
 // поддежрка подключения svg во всех браузерах
-$(document).ready(function () {
-	svg4everybody();
-	var sandwichClick = function() {
-		$(document).on('click', '.catalog-nav__header', function(){
-			var sandwich = $(this).find('.sandwich');
-			sandwich.toggleClass('is-active')
-		})
-	}
-	sandwichClick();
+$(document).ready(function() {
+  svg4everybody();
+  var sandwichClick = function() {
+    $(document).on("click", ".catalog-nav__header", function() {
+      var sandwich = $(this).find(".sandwich");
+      sandwich.toggleClass("is-active");
+    });
+  };
+  $(document).on("click", ".sidebar__header", function() {
+    var sidebarItem = $(this).closest(".sidebar__item");
+    sidebarItem.toggleClass("is-active");
+  });
+  $(document).on("click", ".sidebar-btn__all", function() {
+    var sidebarContent = $(this).closest(".sidebar__content");
+    sidebarContent.toggleClass("is-active");
+  });
+
+  sandwichClick();
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var stepsSlider = document.getElementById("filter-price");
+  var inputMin = document.getElementById("filter-price__min");
+  var inputMax = document.getElementById("filter-price__max");
+  var inputs = [inputMin, inputMax];
+
+  noUiSlider.create(stepsSlider, {
+    start: [38.72, 971.62],
+    connect: true,
+    margin: 100,
+    format: wNumb({
+      decimals: 2,
+      prefix: "$ "
+    }),
+    range: {
+      min: [0],
+      // "10%": [10, 10],
+      // "50%": [80, 50],
+      // "80%": 150,
+      max: 1000
+    }
+  });
+  stepsSlider.noUiSlider.on("update", function(values, handle) {
+    inputs[handle].value = values[handle];
+  });
+  // Listen to keydown events on the input field.
+  inputs.forEach(function(input, handle) {
+    input.addEventListener("change", function() {
+      stepsSlider.noUiSlider.setHandle(handle, this.value);
+    });
+    input.addEventListener("keydown", function(e) {
+      var values = stepsSlider.noUiSlider.get();
+      var value = Number(values[handle]);
+      // [[handle0_down, handle0_up], [handle1_down, handle1_up]]
+      var steps = stepsSlider.noUiSlider.steps();
+      // [down, up]
+      var step = steps[handle];
+      var position;
+    });
+  });
 });
